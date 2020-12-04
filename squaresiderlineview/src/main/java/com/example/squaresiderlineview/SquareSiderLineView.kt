2f361d75d4da4e77bc31d63f7cdba8bd94afb0c6
Anwesh43 <anweshthecool0@gmail.com>
2020-12-04 14:11:10 +0530

    adding SSLNode class

--- squaresiderlineview/src/main/java/com/example/squaresiderlineview/SquareSiderLineView.kt
+++ squaresiderlineview/src/main/java/com/example/squaresiderlineview/SquareSiderLineView.kt
@@ -117,4 +117,45 @@ class SquareSiderLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SSLNode(var i : Int, val state : State = State()) {
+
+        private var next : SSLNode? = null
+        private var prev : SSLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SSLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSSLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SSLNode {
+            var curr : SSLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
