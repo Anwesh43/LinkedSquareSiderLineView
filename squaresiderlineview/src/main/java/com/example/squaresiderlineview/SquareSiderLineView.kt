45f85becd030db99d2868b5366813ca79a27af2a
Anwesh43 <anweshthecool0@gmail.com>
2020-12-04 12:54:09 +0530

    adding constants and libraries

--- squaresiderlineview/src/main/java/com/example/squaresiderlineview/SquareSiderLineView.kt
+++ squaresiderlineview/src/main/java/com/example/squaresiderlineview/SquareSiderLineView.kt
@@ -1,2 +1,27 @@
 package com.example.squaresiderlineview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#03A9F4",
+    "#4CAF50",
+    "#FF9800",
+    "#3F51B5"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val rot : Float = 90f
+val backColor : Int = Color.parseColor("#BDBDBD")
