1baa4920e505445fe69a7fb64bbbd0f7cb41736b
Anwesh43 <anweshthecool0@gmail.com>
2020-12-04 13:52:54 +0530

    adding SquareSiderLineView class

--- squaresiderlineview/src/main/java/com/example/squaresiderlineview/SquareSiderLineView.kt
+++ squaresiderlineview/src/main/java/com/example/squaresiderlineview/SquareSiderLineView.kt
@@ -54,3 +54,19 @@ fun Canvas.drawSSLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawSquareSiderLine(scale, w, h, paint)
 }
+
+class SquareSiderLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
