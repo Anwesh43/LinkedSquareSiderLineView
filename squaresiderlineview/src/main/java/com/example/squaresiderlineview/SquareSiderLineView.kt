f506191b588ab6ed09b6cdefc2229989bacdf1a3
Anwesh43 <anweshthecool0@gmail.com>
2020-12-04 13:42:36 +0530

    adding Canvas extension methods

--- squaresiderlineview/src/main/java/com/example/squaresiderlineview/SquareSiderLineView.kt
+++ squaresiderlineview/src/main/java/com/example/squaresiderlineview/SquareSiderLineView.kt
@@ -30,3 +30,27 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawSquareSiderLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2, h / 2)
+    rotate(rot * sf4)
+    drawLine(0f, 0f, -size * 0.5f * sf1, 0f, paint)
+    drawLine(-size * 0.5f, 0f, -size * 0.5f, -size * 0.5f * sf2, paint)
+    drawRect(RectF(-size * 0.5f - size * 0.5f * sf3, -size * 0.5f, -size * 0.5f, 0f), paint)
+    restore()
+}
+
+fun Canvas.drawSSLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    drawSquareSiderLine(scale, w, h, paint)
+}
