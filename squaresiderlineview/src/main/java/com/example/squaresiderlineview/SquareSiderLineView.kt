d7d6ad1baf1fdeb1a9c700743ed1c89acf3165fa
Anwesh43 <anweshthecool0@gmail.com>
2020-12-04 14:12:50 +0530

    adding SquareSiderLine class

--- squaresiderlineview/src/main/java/com/example/squaresiderlineview/SquareSiderLineView.kt
+++ squaresiderlineview/src/main/java/com/example/squaresiderlineview/SquareSiderLineView.kt
@@ -158,4 +158,27 @@ class SquareSiderLineView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class SquareSiderLine(var i : Int) {
+
+        private var curr : SSLNode = SSLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
